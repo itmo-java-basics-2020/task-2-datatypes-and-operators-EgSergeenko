590f05cc20c996a9060f19a46bc920c67ab748ed
EgSergeenko <es.egor.sergeenko@gmail.com>
2020-02-20 17:41:32 +0300

    init commit

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -10,22 +10,26 @@ public class Task2 {
      * - дано 17.9
      * - вывод 0.9
      */
-    double getFractionalPart(double realNumber) {
-        return 0.0; //your code here
+    double getFractionalPart(double realNumber)
+    {
+        return realNumber % 1;
     }
 
     /**
      * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
      */
-    int charCode(char c) {
-        return 0; //your code here
+    int charCode(char c)
+    {
+        int code = c;
+        return code;
     }
 
     /**
      * Даны 2 целых числа. Проверьте равны ли они
      */
-    boolean testIfIntsEqual(Integer a, Integer b) {
-        return false; //your code here
+    boolean testIfIntsEqual(Integer a, Integer b)
+    {
+        return a.equals(b);
     }
 
     /**
@@ -37,36 +41,73 @@ public class Task2 {
      * @param rightBound  верхняя граница диапазона
      * @param inclusively входят ли границы в заданный даипазон
      */
-    boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively) {
-        return false; //your code here
+    boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively)
+    {
+        if (number != null && leftBound != null && rightBound != null && inclusively != null)
+        {
+            if (inclusively)
+            {
+                if (number >= leftBound && number <= rightBound)
+                {
+                    return true;
+                }
+            }
+            if (number > leftBound && number < rightBound)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
      * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
      */
     boolean atLeastOneIsDigit(char c1, char c2, char c3) {
-        return false; //your code here
+        if (Character.isDigit(c1) || Character.isDigit(c2) || Character.isDigit(c3))
+        {
+            return true;
+        }
+        return false;
     }
 
     /**
      * Даны два вещественных числа. Определите равны ли они
      */
-    boolean areRealNumbersEqual(double a, double b) {
-        return false; //your code here
+    boolean areRealNumbersEqual(double a, double b)
+    {
+        return Math.abs(a - b) < 1e-5;
     }
 
     /**
      * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
      */
-    int sumOfDigits(int n) {
-        return 0; //your code here
+    int sumOfDigits(int n)
+    {
+        int ones = 0;
+        int tens = 0;
+        int hundreds = 0;
+        ones = n % 10;
+        n = (n - ones) / 10;
+        tens = n % 10;
+        n = (n - tens) / 10;
+        hundreds = n % 10;
+        return ones + tens + hundreds;
     }
 
     /**
      * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
      */
-    int nextEvenNumber(int n) {
-        return 0; //your code here
+    int nextEvenNumber(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return  n + 2;
+        }
+        else
+        {
+            return n + 1;
+        }
     }
 
     /**
@@ -75,15 +116,47 @@ public class Task2 {
      * Выведите минимальное кол-во парт, которые нужно приобрести школе, при условии что каждый из классов сидит в
      * своем кабинете
      */
-    int schoolDesks(int num1, int num2, int num3) {
-        return 0; //your code here
+    int schoolDesks(int num1, int num2, int num3)
+    {
+        int sum = 0;
+        if (num1 % 2 == 0)
+        {
+            sum += num1 / 2;
+        }
+        else
+        {
+            sum += (num1 + 1) / 2;
+        }
+        if (num2 % 2 == 0)
+        {
+            sum += num2 / 2;
+        }
+        else
+        {
+            sum += (num2 + 1) / 2;
+        }
+        if (num3 % 2 == 0)
+        {
+            sum += num3 / 2;
+        }
+        else
+        {
+            sum += (num3 + 1) / 2;
+        }
+        return sum;
     }
 
     /**
      * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
      */
-    int xorDigits(int N) {
-        return 0; //your code here
+    int xorDigits(int N)
+    {
+        int x0;
+        int x1;
+        x0 = N % 10;
+        N = (N - x0) / 10;
+        x1 = N % 10;
+        return x0 ^ x1;
     }
 
 }
